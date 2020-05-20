de8fca502e8931e44b6050ae2790eebf446b609b
melon <961255554@qq.com>
2020-05-20 18:07:12 +0800

    配置

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp');
-const { watch,series } = require('gulp');
+const { jwatch,cwatch,series } = require('gulp');
 const uglify=require('gulp-uglify');
 const minicss=require('gulp-minify-css');
 const Cless=require('gulp-less');
-
+const Jwatcher=jwatch(['src/js/*.js']);
+const Cwatcher=cwatch(['src/less/*.less']);
 function js(){
 	return gulp.src('src/js/*.js')
   .pipe(uglify())
@@ -19,6 +20,12 @@ function less(){
   .pipe(Cless())
   .pipe(gulp.dest('src/css'));
 }
+Jwatcher.on('change',function(path,stats){
+	js();
+});
+Cwatcher.on('change',function(path,stats){
+	series(less,css);
+});
 exports.builderall=series(js,less,css)
 exports.JSbuilder=js
 exports.CSSbuilder=css
