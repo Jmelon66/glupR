aa2ab1ea13a22c4ca4f1025d1d87e9dd483c5767
melon <961255554@qq.com>
2020-05-20 17:46:02 +0800

    gulp配置

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,25 @@
-var gulp = require('gulp')
-var uglify=require('gulp-uglify')
-var minicss=require('gulp-minigy-css')
-gulp.task('script',()=>{
+var gulp = require('gulp');
+const { watch,series } = require('gulp');
+const uglify=require('gulp-uglify');
+const minicss=require('gulp-minify-css');
+const Cless=require('gulp-less');
+
+function js(){
 	return gulp.src('src/js/*.js')
-	.pipe(uglify())
-	.pipe(gulp.dest('dist/js'))
-})
+  .pipe(uglify())
+  .pipe(gulp.dest('dist/js'));
+}
+function css(){
+	return gulp.src('src/css/*.css')
+  .pipe(minicss())
+  .pipe(gulp.dest('dist/css'));
+}
+function less(){
+	return gulp.src('src/less/*.less')
+  .pipe(Cless())
+  .pipe(gulp.dest('src/css'));
+}
+exports.builderall=series(js,less,css)
+exports.JSbuilder=js
+exports.CSSbuilder=css
+exports.lessbuilder=less
